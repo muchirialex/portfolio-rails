b2cc2e6796fc1fe7fc263a41e9f3d9b2c11c2c9a
alex-muchiri <muchiri94@gmail.com>
2018-03-21 03:33:19 +0300

    Added sign_up functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://serene-beach-89427.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://muchirialex.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
@@ -102,5 +102,4 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-
 end
