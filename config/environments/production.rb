085233083f0cad0af4520feb724ba2d04a2c80ae
alex-muchiri <muchiri94@gmail.com>
2018-03-21 03:25:09 +0300

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://muchirialex.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://serene-beach-89427.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
@@ -103,14 +103,4 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  # config/environments/production.rb
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
 end
